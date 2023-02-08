498f546a27a322246244bab7c6f0aca2e09158c0
ANDERSON RODRIGO <andersonhav@gmail.com>
2023-02-08 01:30:08 -0300

    zz

---
+++ script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector(".menu")
+const hamb = document.querySelector(".hamb")
+const close = document.querySelector(".close")
+
+console.log(document.body)
+
+hamb.addEventListener("click", () => {
+    menu.style.display = "block"
+    close.style.display = "block"
+    hamb.style.display = "none"
+    document.body.style.overflow = "hidden"
+})
+
+close.addEventListener("click", () => {
+    menu.style.display = "none"
+    close.style.display = "none"
+    hamb.style.display = "block"
+    document.body.style.remove = "overflow"
+})
